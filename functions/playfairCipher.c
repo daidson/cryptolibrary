f08b6665062b8180a89f6546d6c33f523405aa7c
Daidson Alves <daidson.alves@gmail.com>
2020-09-17 22:01:54 -0300

    Inserting functions in playfair

--- functions/playfairCipher.c
+++ functions/playfairCipher.c
@@ -16,6 +16,11 @@ int playfairCipher()
 
     startMatrix();
     removeSpaces();
+    removeRepeated();
+    equalizeLetters();
+    saveKeyInMatrix();
+    saveMatrixElements();
+    printMatrix();
 }
 
 void startMatrix()
@@ -100,7 +105,7 @@ void equalizeLetters()
     char ch = 'a';
     int i = 0, j = 0, a = 0, b = 0, temp1 = 0;
     int l, temp = 0, flag;
-    
+
     while (key[temp] != '\0')
     {
         if (key[temp] == 'i' || key[temp] == 'j')
@@ -120,5 +125,97 @@ void equalizeLetters()
             }
         }
         temp++;
+    }
+}
+
+void saveKeyInMatrix()
+{
+    //Salvando chave na matriz
+    char ky[50], pt[100], key[50];
+    char m[5][5];
+    char ch = 'a';
+    int i = 0, j = 0, a = 0, b = 0, temp1 = 0;
+    int l, temp = 0, flag;
+
+    while (key[temp] != '\0')
+    {
+        if (b == 5)
+        {
+            b = 0;
+            a++;
+        }
+        m[a][b] = key[temp];
+        temp++;
+        b++;
+    }
+}
+
+void saveMatrixElements()
+{
+    char ky[50], pt[100], key[50];
+    char m[5][5];
+    char ch = 'a';
+    int i = 0, j = 0, a = 0, b = 0, temp1 = 0;
+    int l, temp = 0, flag;
+
+    //Salvando os elementos na matriz exceto pela chave
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            temp = 0;
+            flag = 0;
+            while (key[temp] != '\0')
+            {
+                if (key[temp] == ch)
+                {
+                    ch++;
+                    flag = 1;
+                    break;
+                }
+                if (ch == 'i' || ch == 'j')
+                {
+                    for (a = 0; a < temp; a++)
+                    {
+                        if (key[temp] == 'i' || key[temp] == 'j')
+                        {
+                            ch++;
+                            flag = 1;
+                            break;
+                        }
+                    }
+                }
+                temp++;
+            }
+            if (flag == 0 && m[i][j] == 0)
+            {
+                m[i][j] = ch;
+                ch++;
+                if (ch == 'i')
+                    ch++;
+            }
+            if (m[i][j] == 0)
+                j--;
+        }
+    }
+}
+
+void printMatrix()
+{
+    char ky[50], pt[100], key[50];
+    char m[5][5];
+    char ch = 'a';
+    int i = 0, j = 0, a = 0, b = 0, temp1 = 0;
+    int l, temp = 0, flag;
+
+    //Mostra a matriz em tela
+    printf("\n\nMatriz:\n\n");
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            printf("  %c", m[i][j]);
+        }
+        printf("\n");
     }
 }

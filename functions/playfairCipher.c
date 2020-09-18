d24bd4aa06986c0c7cef961da0cbd96200a58787
Daidson Alves <daidson.alves@gmail.com>
2020-09-17 21:49:54 -0300

    Creating head of the function

---
+++ functions/playfairCipher.c
@@ -0,0 +1,24 @@
+void encrypt(char pt[], char mat[]);
+
+int playfairCipher() {
+    char ky[50], pt[100], key[50];
+    char m[5][5];
+    // char mat[25];
+    char ch = 'a';
+    int i = 0, j = 0, a = 0, b = 0, temp1 = 0;
+    int l, temp = 0, flag;
+     
+    printf("\nEntre com a chave: \n");
+    gets(ky);
+    printf("\nEntre com o texto a ser criptografado: \n");
+    gets(pt);
+
+    //iniciando a matriz no ponto 0
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            m[i][j] = 0;
+        }
+    }
+}
